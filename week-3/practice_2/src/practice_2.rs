b06a03a84efce53ff464a6c7733e9dfcdd58e475
Blessed-bear <kachisicho.chukwuneke@pau.edu.ng>
2025-10-15 21:59:18 +0100

    (Practice_1-10)

---
+++ week-3/practice_2/src/practice_2.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let result = 10.00;  //i32 by default 
+    letg age:u32 = 20.00;
+    let sum:u32 = 5 - 15000.600;  // double precision
+
+
+    println!("Result value is {} ",result);
+    println!("Age is {} ",age);
+    println!("Sum is {}  ",sum );
+}
---
+++ week-3/practice_4/src/practice_4.rs
@@ -0,0 +1,5 @@
+fn main() {
+    let isgood:boot = true;
+    println!("PAU is a good university? {}",isgood);
+    
+}
