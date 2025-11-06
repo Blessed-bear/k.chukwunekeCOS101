63a2a7affa422f2516489603844a9d206ebd123e
Blessed-bear <kachisicho.chukwuneke@pau.edu.ng>
2025-11-06 06:20:13 +0100

    Week_6 practices

---
+++ week_6/practice_11/target/debug/practice_11.d
@@ -0,0 +1,1 @@
+C:\Users\HomePC\Documents\k.chukwunekeCOS101\week_6\practice_11\target\debug\practice_11.exe: C:\Users\HomePC\Documents\k.chukwunekeCOS101\week_6\practice_11\src\main.rs
---
+++ week_6/practice_9/target/debug/practice_9.d
@@ -0,0 +1,1 @@
+C:\Users\HomePC\Documents\k.chukwunekeCOS101\week_6\practice_9\target\debug\practice_9.exe: C:\Users\HomePC\Documents\k.chukwunekeCOS101\week_6\practice_9\src\main.rs
